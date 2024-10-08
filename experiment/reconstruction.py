fd77cf51b217b21debfd30a68e0ed6666ea8aedd
Florian Faltermeier <florifal@posteo.de>
2024-10-08 16:34:49 +0200

    Add timeout for reconstruction and exception for damaged output models

--- experiment/reconstruction.py
+++ experiment/reconstruction.py
@@ -1,15 +1,28 @@
 import json
 import shutil
-from datetime import datetime
+import subprocess
+import time
+from datetime import datetime, timedelta
 from pathlib import Path
 
 from experiment import global_vars as glb
-from experiment.utils import crs_url_from_epsg, is_numeric, execute_subprocess
+from experiment.utils import crs_url_from_epsg
+
+
+class ReconstructionError(Exception):
+    pass
 
 
 class Reconstruction:
 
-    def __init__(self, crs: str, config: dict, cloud_filepath: str = ""):
+    def __init__(
+            self,
+            crs: str,
+            config: dict,
+            cloud_filepath: str = "",
+            add_geoflow_params: dict | None = None,
+            geoflow_timeout: int | None = None
+    ):
         self.crs = crs
         self.config = config
 
@@ -20,6 +33,9 @@ class Reconstruction:
         self.config_toml_filepath = self.output_dirpath / "config.toml"
         date_time = datetime.today().strftime("%y%m%d-%H%M%S")
         self.geoflow_log_filepath = self.output_dirpath / f"geoflow_log_{date_time}.txt"
+
+        self.add_geoflow_params = {} if add_geoflow_params is None else add_geoflow_params
+        self.geoflow_timeout = geoflow_timeout
 
         self.executor: ReconstructionExecutor | None = None
 
@@ -46,37 +62,24 @@ class Reconstruction:
             "input_footprint_sql": self.config["building_footprints_sql"],
             "building_identifier": self.config["building_identifier"],
             "input_pointcloud": self.cloud_filepath,
-            **{k: v for k, v in self.geoflow_parameters.items() if v is not None}
+            **{k: v for k, v in self.geoflow_parameters.items() if v is not None},
+            **self.add_geoflow_params
         }
 
         # Write the config.toml
         with open(self.config_toml_filepath, "w", encoding="utf-8") as f:
             for key, value in config_toml_json.items():
-                # Numeric values (int and float) should not be enclosed in single quotation marks, strings should
-                if is_numeric(value):
-                    # Make sure integers are written without decimal places
-                    if int(value) == float(value):
-                        value_str = str(int(value))
-                    else:
-                        value_str = str(float(value))
+                try:
+                    param_type = glb.geoflow_parameter_types[key]
+                except KeyError:
+                    value_str = f"'{str(value)}'"
                 else:
-                    value_str = f"'{value}'"
-                f.write(f"{key}={value_str}\n")
+                    if param_type is str:
+                        value_str = f"'{str(value)}'"
+                    else:
+                        value_str = str(param_type(value)).lower()  # .lower() to makes booleans lowercase
 
-        # copy reconstruct_template.json to output_dir
-        # (because if calling geof with argument -w, the output directories will be considered
-        # to be relative to the directory that contains the json. this also makes it unnecessary
-        # to include all the different output paths into the config.toml)
-        #
-        # create config.toml to include the following params:
-        # - GF_PROCESS_CRS, input_pointcloud_crs_wkt, output_crs_wkt <- all crs
-        # - output_cj_referenceSystem <- update URL with epsg code from crs
-        # - input_footprint
-        # - building_identifier
-        # - input_pointcloud
-        #
-        # or potentially: even simply edit the JSON, should not be much harder, only less convenient
-        # to check the settings later on
+                f.write(f"{key}={value_str}\n")
 
     def setup_executor(self):
         # or combine this method with run()
@@ -87,7 +90,8 @@ class Reconstruction:
             geoflow_json_filepath=self.geoflow_json_filepath,
             config_toml_filepath=self.config_toml_filepath,
             cmd_options=f"--verbose --workdir --config {self.config_toml_filepath.as_posix()}",
-            stdout_log_filepath=self.geoflow_log_filepath
+            stdout_log_filepath=self.geoflow_log_filepath,
+            geoflow_timeout=self.geoflow_timeout
         )
 
     def run(self):
@@ -129,7 +133,8 @@ class ReconstructionExecutor:
             stdout_log_filepath: Path | str,
             config_toml_filepath: Path | str = "",
             cmd_options: str | list[str] = "",
-            very_verbose: bool = False
+            very_verbose: bool = False,
+            geoflow_timeout: int | None = None
     ):
         self.geoflow_cmd = geoflow_cmd
         self.stdout_log_filepath = Path(stdout_log_filepath)
@@ -137,6 +142,7 @@ class ReconstructionExecutor:
         # Converting backslashes to slashes because not sure if subprocess.run() can handle escaped backslashes
         self.geoflow_json_filepath = Path(geoflow_json_filepath).as_posix()
         self.config_toml_filepath = Path(config_toml_filepath).as_posix()
+        self.geoflow_timeout = geoflow_timeout
 
         # Command line options may be passed as single string or as list of strings. If they are a single string, they
         # must be converted to a list of strings to serve as argument for subprocess.run().
@@ -166,20 +172,16 @@ class ReconstructionExecutor:
             self.command.extend(self.cmd_options)
 
     def run_geoflow(self):
-        print("Starting 3D building reconstruction ...\n")
+        print("\nStarting 3D building reconstruction ...")
         print(f"- Command: {' '.join(self.command)}")
         print(f"- Output log file: {str(self.stdout_log_filepath)}")
+        t0 = time.time()
 
-        if not self.very_verbose:
-            with open(self.stdout_log_filepath, "w", encoding="utf-8") as f:
-                for stdout_line in execute_subprocess(self.command):
-                    f.write(stdout_line)
-        else:
-            print("")
-            with open(self.stdout_log_filepath, "w", encoding="utf-8") as f:
-                for stdout_line in execute_subprocess(self.command):
-                    print(stdout_line, end="")
-                    f.write(stdout_line)
+        with open(self.stdout_log_filepath, "w", encoding="utf-8") as f:
+            try:
+                subprocess.run(self.command, stdout=f, stderr=subprocess.STDOUT, text=True, timeout=self.geoflow_timeout, check=True)
+            except TimeoutError as e:
+                raise TimeoutError(e)
 
-        print("")
-        print("Finished 3D building reconstruction.")
+        t1 = time.time()
+        print(f"\nFinished 3D building reconstruction after {str(timedelta(seconds=t1 - t0))}.")
